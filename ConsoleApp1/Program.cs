a148eccf9fd76f440906a9ef8c0ec7acde299003
noodle <khshum8@yahoo.com.au>
2020-01-03 20:03:44 +1000

    bubbleSort_code in Blind

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,154 +5,70 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using System.IO;
-using System.Diagnostics;
 
-
-namespace bubbleSort
+namespace Sort
 {
-    //How to Measure Code Execution Time In C#
-    //https://www.youtube.com/watch?v=KfpqlX57kuI
-
     class Program
     {
+        //write a visualisation version of this 
+        //still not clear how the for loop within the for loop do?
+
+
         static void Main()
         {
-            Stopwatch MyTimer = new Stopwatch();
-            
-            //int[] unsortedList = { 0, 4, 3, 1, 10, 7 };// Why I keep forget about array?
-            int[] unsortedList = { 10, 4, 3, 1, 7, 0 };// Why I keep forget about array?
-            //int[] unsortedList = { 2, 7, 4, 1, 5, 3};
-            //int[] unsortedList = { 1, 2, 3, 4, 5, 6 };
-            int testOG = 0;//how to pass by reference and value 
+            //int[] array = { 10, 4, 3, 1, 5, 0 };
+            int[] array = { 0, 1, 3, 4, 5, 10 };
 
-            MyTimer.Start();
+            BubbleSort one = new BubbleSort();
+            one.sort(array);
 
-            foreach (int items in unsortedList)
+            foreach (int item in array)
             {
-                Console.WriteLine(items);
+                Console.WriteLine(item);
             }
-
-            bubbleSort bubble = new bubbleSort();
-            bubble.BubbleSort(unsortedList, testOG);//still the same?
-
             //Console.ReadLine();
-
-            foreach (int items in unsortedList)//reference issues?
-            {
-                Console.WriteLine(items);
-            }
-
-            MyTimer.Stop();
-            //Console.WriteLine("testOG: " + testOG);
-            //Console.ReadLine();
-            Console.WriteLine("time taken: " + MyTimer.Elapsed);
         }
 
-
-        //class bubbleSort//Class within a class? How does that work? Why?
-        //{
-        //    public void BubbleSort(int[] unsortedList)
-        //    {
-        //        int temp;
-
-        //        for (int i = 0; i < unsortedList.Length - 1; i++)
-        //        {
-        //            for (int j = 0; j < unsortedList.Length - (1 + i); j++)
-        //            {
-        //                if (unsortedList[j] > unsortedList[j + 1])
-        //                {
-        //                    temp = unsortedList[j + 1];
-        //                    unsortedList[j + 1] = unsortedList[j];
-        //                    unsortedList[j] = temp;
-
-        //                }
-
-        //            }
-        //        }
-        //    }
-        //}
-
-
-        //public static void BubbleSort(int[] unsortedList)
-        //{
-        //    int temp;
-
-        //    for (int i = 0; i < unsortedList.Length-1; i++)
-        //    {
-        //        for (int j = 0; j < unsortedList.Length-(1 + i); j++)
-        //        {
-        //            if (unsortedList[j] > unsortedList[j+1])
-        //            {
-        //                temp = unsortedList[j + 1];
-        //                unsortedList[j + 1] = unsortedList[j];
-        //                unsortedList[j] = temp;
-
-        //            }
-
-        //        }
-        //    }
-        //}
     }
 
-    class bubbleSort
+    class BubbleSort//class don't have signiture 
     {
-        //pass by reference? 
+        
 
-        public void BubbleSort(int[] unsortedList, int testOG)
+        public void sort(int[] array)
         {
             int temp;
-            int count = 0;
+            int count=0;
 
-            for (int i = 0; i < unsortedList.Length - 1; i++)// -1 will prevent the program crash but will it skip a loop and return incomplete result?
+            for (int i = 0; i < array.Length - 1; i++)
             {
-                //Console.WriteLine();
-                //Console.WriteLine("i outside forLoop: " + i);
-                //Console.WriteLine("Length: " + unsortedList.Length);
-
-                //int flag = 0; 
-
-                for (int j = 0; j < unsortedList.Length - (1 + i); j++)//2 + 1 length go 
+                for (int j = 0; j < array.Length - (1 + i); j++)
                 {
-                    //Console.WriteLine();
-                    //int forLoopLen = unsortedList.Length - (1 + i);
-                    //int forLoopLen = unsortedList.Length;
-                    //Console.WriteLine("i inside forLoop: " + i);
-                    //Console.WriteLine("j: " + j);
-                    //Console.WriteLine("length: " + forLoopLen);
-
-                    if (unsortedList[j] > unsortedList[j + 1])//compare to the next one
+                    if (array[j] > array[j + 1])
                     {
-
-                        temp = unsortedList[j + 1];
-                        unsortedList[j + 1] = unsortedList[j];
-                        unsortedList[j] = temp;
-
-                        //Console.WriteLine("temp ifLoop: " + temp);
-
-                        //Console.WriteLine("unsort[1] ifLoop: " + unsortedList[1]);
-                        //Console.WriteLine("unsort[2] ifLoop: " + unsortedList[2]);
-                        //Console.WriteLine("unsort[3] ifLoop: " + unsortedList[3]);
-                        //int[] unsortedList = { 0, 4, 3, 1, 10, 7 }; 0, 1, 3, 4, 7, 10
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        //Console.WriteLine(array[j]);
                         count = count + 1;
-                        //flag = 1;
+
+
                     }
 
-                }//for
+                }
 
                 if (count == 0)
-                {                    
+                {
+                    //Console.WriteLine("break");
                     break;
-                };
-
-                
-                Console.WriteLine("Count: " + count);
-
-            }//for 
-
-            //testOG = testOG + 2;
-            
-            //Console.WriteLine("testOG Inside Class BubbleSort: " + testOG);
+                    //Console.WriteLine("break");
+                }
+                Console.WriteLine("break");
+            }
+            Console.WriteLine("break");
+            Console.WriteLine("count: " + count);
         }
     }
 
 }
+
