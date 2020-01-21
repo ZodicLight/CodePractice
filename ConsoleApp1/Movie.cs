621b4c092cf9672e0f2d649fb11e76ee766622d8
noodle <khshum8@yahoo.com.au>
2020-01-22 03:03:31 +0800

    getter setter done

---
+++ ConsoleApp1/Movie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Movie
+    {
+        public string title;
+        public string director;
+        //public string rating;
+        private string _rating;//getter and setter
+
+        public Movie(string aTitle, string aDirector, string aRating)//constructor 
+        {
+            title = aTitle;
+            director = aDirector;
+            //rating = aRating;
+            Rating = aRating;//Rating is calling the method Rating 
+        }
+
+        public string Rating
+        {
+            get { return _rating; }
+
+            set
+            {
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "NR")
+                {
+                    _rating = value;
+                }
+                else
+                {
+                    _rating = "NR";
+                }
+            }
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,45 +6,24 @@ using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using System.IO;
 
-namespace OOP
+namespace Giraffe
 {
-    //Encapsulation 
-    //object include property and method 
-
     class Program
     {
-        public static void Main()
+        static void Main(string[] args)
         {
-            //Procedural programing 
-            ////////////////////////
-            //int baseSalary = 30000;
-            //int overtime = 10;
-            //int rate = 20; 
-
-            //function getWage(baseSalary, overtime, rate)
-            //{
-            //    return baseSalary + (overtime * rate);
-            //}
-
-            Employee kevin = new Employee();
-            //kevin.GetWage();
-            Console.WriteLine(kevin.GetWage());
-            Console.ReadLine();
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "Dog");
+            //Movie avengers = new Movie("The Avengers", "Joss Whedon", "PG-13");
+            Movie shrek = new Movie("Shrek", "Adam Adamson", "PG");
+            //Rating: G, PG, PG-13, R , NR
+            //avengers.rating = "dog";//this is bad 
+            shrek.Rating = "R"; //should return NR??
+            Console.WriteLine(shrek.Rating);
 
         }
-    }
 
-    public class Employee
-    {
-        public int BaseSalary = 30000;
-        public int Overtime = 10;
-        public int Rate = 20;
-        
-        public int GetWage()
-        {
-            return this.BaseSalary + (this.Overtime * this.Rate);
-        } 
-    }
 
+    }
+    
 }
 
